2df128bed7beab49214a09c309adde87fbaa979b
Din <hurshid.17.97@mail.ru>
2023-02-23 23:33:34 +0500

    Main

---
+++ Main.java
@@ -0,0 +1,14 @@
+import Unit.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Fermer fermer1 = new Fermer("Sem", 100, 1, 1,1);
+        Bandit bandit1 = new Bandit("Vor", 100, 10,5, 50);
+        Sniper sniper1 = new Sniper("Sever", 100, 50,20,50);
+        Witch witch1 =new Witch("Kokos", 100, 30, 30, 20);
+        Spearman spearman1 = new Spearman("Arik", 100, 30, 30, 10);
+        Crossbowman crossbowman1 = new Crossbowman("Boy", 100, 40,20, 30);
+        Monk monk1 = new Monk("Lari", 100, 5, 5, 30);
+
+    }
+}
